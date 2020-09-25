bbfaebc399f9845af1e1c35c9e25a7db06e04c98
Thiago Oliveira <thiagooliveira1706@gmail.com>
2020-09-25 15:57:26 -0300

    Ajustes campos Ano - Periodo / Delete campo anoInicio

--- app/forms.py
+++ app/forms.py
@@ -4,9 +4,5 @@ from .models import CadastroEgresso
 class CadastroEgressoForm(forms.ModelForm):
 	class Meta:
 		model = CadastroEgresso
-		#exclude = 
 		fields =  '__all__'
 			
-	WIDGETS = [
-
-	]
